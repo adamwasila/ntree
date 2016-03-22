360aa0b98ecad93d993f88a53224a41889514bd0
Adam Wasila <adam.wasila@gmail.com>
2016-03-22 23:25:11 +0100

    Add UT to differentiate between insert and add operations on indexed tree

--- src/test/java/org/wasila/ntree/IndexedNTreeTest.java
+++ src/test/java/org/wasila/ntree/IndexedNTreeTest.java
@@ -35,4 +35,64 @@ public class IndexedNTreeTest {
         Assert.assertEquals(1, tree.getChildrenCount(0, 0));
     }
 
+    @Test
+    public void addToIndexedTree() {
+        IndexedNTree<String> tree = new IndexedNTreeImpl<>();
+        tree.addChild("root");
+        tree.addChild("firstChild", 0);
+        tree.addChild("secondChild", 0);
+        tree.addChild("thirdChild", 0);
+
+        Assert.assertEquals("root", tree.get());
+        Assert.assertEquals(3, tree.getChildrenCount(0));
+        Assert.assertEquals("firstChild", tree.get(0, 0));
+        Assert.assertEquals("secondChild", tree.get(0, 1));
+        Assert.assertEquals("thirdChild", tree.get(0, 2));
+    }
+
+    @Test
+    public void insertToIndexedTree() {
+        IndexedNTree<String> tree = new IndexedNTreeImpl<>();
+        tree.addChild("root");
+        tree.insertChild("firstChild", 0, 0);
+        tree.insertChild("secondChild", 0, 1);
+        tree.insertChild("thirdChild", 0, 2);
+
+        Assert.assertEquals("root", tree.get());
+        Assert.assertEquals(3, tree.getChildrenCount(0));
+        Assert.assertEquals("firstChild", tree.get(0, 0));
+        Assert.assertEquals("secondChild", tree.get(0, 1));
+        Assert.assertEquals("thirdChild", tree.get(0, 2));
+    }
+
+    @Test
+    public void insertToIndexedTreeInReversedOrder() {
+        IndexedNTree<String> tree = new IndexedNTreeImpl<>();
+        tree.addChild("root");
+        tree.insertChild("thirdChild", 0, 0);
+        tree.insertChild("secondChild", 0, 0);
+        tree.insertChild("firstChild", 0, 0);
+
+        Assert.assertEquals("root", tree.get());
+        Assert.assertEquals(3, tree.getChildrenCount(0));
+        Assert.assertEquals("firstChild", tree.get(0, 0));
+        Assert.assertEquals("secondChild", tree.get(0, 1));
+        Assert.assertEquals("thirdChild", tree.get(0, 2));
+    }
+
+    @Test
+    public void insertToIndexedTreeInMixedOrder() {
+        IndexedNTree<String> tree = new IndexedNTreeImpl<>();
+        tree.addChild("root");
+        tree.insertChild("thirdChild", 0, 0);
+        tree.insertChild("firstChild", 0, 1);
+        tree.insertChild("secondChild", 0, 1);
+
+        Assert.assertEquals("root", tree.get());
+        Assert.assertEquals(3, tree.getChildrenCount(0));
+        Assert.assertEquals("firstChild", tree.get(0, 0));
+        Assert.assertEquals("secondChild", tree.get(0, 1));
+        Assert.assertEquals("thirdChild", tree.get(0, 2));
+    }
+
 }
